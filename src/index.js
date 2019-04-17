f7b13acba08dcb71eeb4df6024f11e90343d244b
CorradoRossi <rossimeacham@gmail.com>
2019-04-17 19:55:29 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('offline-plugin/runtime').install();
+
+import './index.html';
+import './index.scss';
+import './js/vendor/vendor.js';
+import './js/vendor/jquery.js';
+//import './js/main.js';
